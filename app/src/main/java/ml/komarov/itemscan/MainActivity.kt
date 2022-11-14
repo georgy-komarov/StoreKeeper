3d42493a03f2a63e3d8ac32d81e6df41d92bf8cf
Georgy Komarov <georgy.komarov@mail.ru>
2022-11-15 02:53:17 +0300

    Move menu_settings from activity to BaseSelectorFragment

--- app/src/main/java/ml/komarov/itemscan/MainActivity.kt
+++ app/src/main/java/ml/komarov/itemscan/MainActivity.kt
@@ -1,18 +1,16 @@
 package ml.komarov.itemscan
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.Toast
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
+import com.google.android.material.navigation.NavigationView
 import ml.komarov.itemscan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -46,19 +44,6 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.action_settings -> navController.navigate(R.id.nav_settings)
-        }
-        return super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/ml/komarov/itemscan/fragments/selectors/BaseSelectorFragment.kt
+++ app/src/main/java/ml/komarov/itemscan/fragments/selectors/BaseSelectorFragment.kt
@@ -3,16 +3,19 @@ package ml.komarov.itemscan.fragments.selectors
 import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.findNavController
 import ml.komarov.itemscan.BarcodeActivity
+import ml.komarov.itemscan.R
 import ml.komarov.itemscan.databinding.FragmentMainBinding
 
 
-abstract class BaseSelectorFragment : Fragment() {
+abstract class BaseSelectorFragment : Fragment(), MenuProvider {
 
     lateinit var productId: String
 
@@ -39,11 +42,32 @@ abstract class BaseSelectorFragment : Fragment() {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
 
         setup()
+        setupMenu()
 
         return binding.root
     }
 
-    private fun setup () {
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_main, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+            R.id.action_settings -> {
+                requireActivity().findNavController(R.id.nav_host_fragment_content_main)
+                    .navigate(R.id.nav_settings)
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun setupMenu() {
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.STARTED)
+    }
+
+    private fun setup() {
         binding.btnScan.setOnClickListener {
             val intent = Intent(activity, BarcodeActivity::class.java)
             resultLauncher.launch(intent)
